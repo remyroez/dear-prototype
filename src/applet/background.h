1d2a723e1a5aba0ff519a217b9a8886e665dfc83
れみろう <remyroez+github@gmail.com>
2020-06-19 23:27:05 +0900

    :sparkles: テスト実装の背景アプレットを実装済みアプレットとして独立

---
+++ src/applet/background.h
@@ -0,0 +1,61 @@
+#ifndef APPLET_BACKGROUND_H_
+#define APPLET_BACKGROUND_H_
+
+#include "dear.h"
+
+namespace applet {
+
+class background : public dear::applet {
+    // 名前
+    virtual const char *name() override {
+        return "background";
+    }
+
+    // インストール
+    virtual void install(dear::application *app) override;
+
+    // 背景画像
+    dear::image _image;
+
+    // 背景サイズ列挙型
+    enum background_size {
+        fixed,
+        fit,
+        cover,
+        contain,
+        custom,
+    };
+
+    // 背景サイズ
+    background_size _size = background_size::fixed;
+
+    // UV
+    ImVec2 _custom_uv0 { 0.f, 0.f };
+    ImVec2 _custom_uv1 { 1.f, 1.f };
+
+public:
+    // 背景画像の読み込み
+    void load_background_image(const char *filename);
+
+    // 背景画像のラップの設定
+    void set_background_image_wrap(sg_wrap wrap_u = _SG_WRAP_DEFAULT, sg_wrap wrap_v = _SG_WRAP_DEFAULT) {
+        _image.wrap_u = wrap_u;
+        _image.wrap_v = wrap_v;
+    }
+
+    // 背景サイズの設定
+    void set_background_size(background_size size) {
+        _size = size;
+    }
+
+    // 背景サイズの設定 (UV指定)
+    void set_background_size(const ImVec2 &uv0 = ImVec2(0.f, 0.f), const ImVec2 &uv1 = ImVec2(1.f, 1.f)) {
+        set_background_size(background_size::custom);
+        _custom_uv0 = uv0;
+        _custom_uv1 = uv1;
+    }
+};
+
+} // namespace applet
+
+#endif // APPLET_BACKGROUND_H_
